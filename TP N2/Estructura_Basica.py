03b4d9ce261594cff8bc7f62f29195eeeb9ff4e4
Camilo Escar <94748462+camilooo95@users.noreply.github.com>
2023-04-12 16:42:08 -0300

    tp2

--- TP N2/Estructura_Basica.py
+++ TP N2/Estructura_Basica.py
@@ -2,7 +2,7 @@
 import openai
 #……. [ mas Código de inicialización aqui ] …………
 
-openai.api_key = "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"
+openai.api_key = "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"
 conversation = ""
 TOP_P=1
 FREQ_PENALTY=0
@@ -12,22 +12,33 @@ MAX_TOKENS=1024
 TEMPERATURE=0.75
 NMAX=1
 MODEL_ENGINE = "text-davinci-003"
-i = 1
+#i = 1
 #…..[otra lógica necesaria – el texto del prompt debe colocarse en userText]…..
 # Set up the model and prompt
-while (i!=0):
+
+#while True (i!=0):
+print("Bienvenido a ChatGPT")
+while True:
     question = input("You: ")
-    conversation += "\nYou: "+ question + "\nchatGPT::"
-    completion = openai.Completion.create(
-        engine=MODEL_ENGINE,
-        prompt=conversation,
-        max_tokens=MAX_TOKENS,
-        n=NMAX,
-        top_p=TOP_P,
-        frequency_penalty=FREQ_PENALTY,
-        presence_penalty=PRES_PENALTY,
-        temperature=TEMPERATURE,
-        stop=STOP)
-    anwer = completion.choices[0].text.strip()
-    conversation += anwer
-    print("chatGPT: " +anwer + "\n")
+    try:
+        conversation += "\nYou: "+ question + "\nchatGPT: "
+        completion = openai.Completion.create(
+            engine=MODEL_ENGINE,
+            prompt=conversation,
+            max_tokens=MAX_TOKENS,
+            n=NMAX,
+            top_p=TOP_P,
+            frequency_penalty=FREQ_PENALTY,
+            presence_penalty=PRES_PENALTY,
+            temperature=TEMPERATURE,
+            stop=STOP)
+        pregunta = completion.choices[0].text.strip()
+        conversation += pregunta
+        print("chatGPT: " +pregunta + "\n")
+    except Exception as e:
+        print("lo siento ha ocurrido un error, Por favor vuelve a intentarlo.")
+        pregunta = completion.choices[0].text.strip()
+
+
+
+    
